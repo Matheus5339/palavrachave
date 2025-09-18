b0146efc2a849aa2b565763a90d5e823869187b2
Matheus5339 <matheustr789@gmail.com>
2025-09-18 17:31:17 -0300

    Create PmupApplication.java

---
+++ pmup/PmupApplication.java
@@ -0,0 +1,11 @@
+package com.pae.pmup;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PmupApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(PmupApplication.class, args);
+    }
+}
